73f5134008a4095dce1b33b3071ff8391e2d6b4c
mudkip989 <muddcop@gmail.com>
2025-07-07 15:06:00 -0500

    Init

---
+++ src/main/java/us/mudkip989/plugins/bge/BGE.java
@@ -0,0 +1,17 @@
+package us.mudkip989.plugins.bge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BGE extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
